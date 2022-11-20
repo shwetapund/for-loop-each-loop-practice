6e40bc5f9931f8e9476022d9323a7ef64de2f4af
shwetapund <pundshweta2020@gmail.com>
2022-11-19 22:22:15 -0800

    for loop practice added

---
+++ for-loop.js
@@ -0,0 +1,23 @@
+
+const arr = ["A", "B","c", "D", "E", "F"]
+
+for(let i=0; i<arr.length; i++)
+{
+    console.log("prasad for =>" + arr[i]);
+}
+/*
+const arr = [1, 2, 3, 4]
+
+for(i=0; i<arr.length; i++)
+{
+    console.log(arr[i]*arr[i])
+}
+*/
+
+const arr1 = ["aniket", "shiv", "harish", "shital"]
+
+for(i=0; i<arr.length; i++)
+{
+    console.log("sending email to:" + arr1[i])
+}
+
